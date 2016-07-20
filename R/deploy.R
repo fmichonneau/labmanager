70e4f3eefc9932ea6c669a943baec607e0d21084
Francois Michonneau <francois.michonneau@gmail.com>
2016-07-19 20:15:18 -0400

    use rsync for deploying

--- R/deploy.R
+++ R/deploy.R
@@ -39,47 +39,33 @@ copy_storr <- function(from = "~/Documents/plankton-larvae-data/data_storr",
 }
 
 copy_photos <- function(from = "~/hdd/plankton-images/app_photos",
-                        to = "~/R-dev/flpk-shiny/www/app_photos",
-                        testing = FALSE) {
+                        to = "~/R-dev/flpk-shiny/www/app_photos") {
 
-    ## use rsync to copy the photos to not do it everytime
     if (!dir.exists(to)) {
         message(sQuote(to), " doesn't exist... Creating it.")
         dir.create(to)
     }
-    to <- normalizePath(to)
     from <- normalizePath(from)
-    src_data <- list.files(path = from, pattern = "jpg$", ignore.case = TRUE,
-                           recursive = TRUE, full.names = TRUE)
-    to_data <- gsub(from, to, src_data)
-    res <- cbind(src_data, to_data)
+    to <- normalizePath(to)
 
-    if (testing) {
-        message("testing... so only copying the first 50 images")
-        res <- res[1:50, ]
-    }
-
-    sapply(to_data, function(x) {
-        x <- dirname(x)
-        if (!dir.exists(x)) {
-            dir.create(x, recursive = TRUE)
-        }
-    })
-
-    apply(res, 1, function(x) file.copy(x[1], x[2], overwrite = TRUE,
-                                        copy.date = TRUE))
+    rsync_cmd <- paste(
+        "rsync -r",
+        from,
+        to
+    )
+    system(rsync_cmd)
 }
 
 ## @param d a droplet
 ## @param testing if \code{TRUE}, only a subset of the images are
 ##     copied to the app to speed up process
 deploy_app <- function(d, app_path = "~/R-dev/flpk-shiny",
-                       testing = FALSE, ...) {
+                       ...) {
 
     ## before deploying we need to copy the photo and the data to www
     copy_data()
     copy_storr()
-    copy_photos(testing = testing)
+    copy_photos()
     ## use rsync to only copy
     #analogsea::docklet_shinyapp(droplet = d, path = app_path,
     #                            img = "fmichonneau/flpk-docker-image",
