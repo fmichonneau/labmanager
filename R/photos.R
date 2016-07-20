263ba3eebad7a908240012d6db8ba2d2dd1f65ce
Francois Michonneau <francois.michonneau@gmail.com>
2016-07-19 20:14:52 -0400

    give UUIDs to photos

--- R/photos.R
+++ R/photos.R
@@ -24,10 +24,16 @@ generate_folder_structure <- function(photo_folder = "/home/francois/hdd/plankto
 ##' @export
 uppercase_photo_extension <- function(photo_folder = "/home/francois/hdd/plankton-images/archive_photos/") {
     lst_files <- list.files(pattern = "jpg$", path = photo_folder, recursive = TRUE, full.names = TRUE)
-    new_nm <- gsub("\\.jpg$", "\\.JPG", lst_files)
-    nm <- cbind(lst_files, new_nm)
-    all(apply(nm, 1, function(x) file.rename(x[1], x[2])))
+
+    if (length(lst_files) > 0) {
+        new_nm <- gsub("\\.jpg$", "\\.JPG", lst_files)
+        nm <- cbind(lst_files, new_nm)
+        all(apply(nm, 1, function(x) file.rename(x[1], x[2])))
+    }
+
 }
+
+
 
 ##' Creates the copies of the photos to be used by the shiny app
 ##'
@@ -38,6 +44,8 @@ uppercase_photo_extension <- function(photo_folder = "/home/francois/hdd/plankto
 generate_thumbnails <- function(archive_folder = "/home/francois/hdd/plankton-images/archive_photos",
                                 app_folder = "/home/francois/hdd/plankton-images/app_photos",
                                 voucher_pattern = "FLPK-[0-9]{4}") {
+
+    uppercase_photo_extension(archive_folder)
 
     ## Get all the directories in the archives
     archive_dirs <- list.files(pattern = voucher_pattern,
@@ -88,4 +96,57 @@ check_photo_creation <- function(original_path, target_path) {
     } else {
         message("DONE.")
     }
+}
+
+
+rename_photos <- function(app_folder = "/home/francois/hdd/plankton-images/app_photos") {
+    ## list all images
+    all_imgs <- list.files(path = app_folder,
+                           pattern = "JPG$",
+                           recursive = TRUE,
+                           ignore.case = TRUE,
+                           full.names = TRUE)
+
+    ## Remove files that have already been renamed
+    all_imgs <- all_imgs[!grepl("^(FLPK-[0-9]{4}_[0-9]{4})",
+                                basename(all_imgs))]
+
+    ## get the unique paths including suffix large/thumbs
+    uniq_paths <- unique(dirname(all_imgs))
+    ## check they all have correct suffix
+    if (!all(grepl("(large|thumbs)$", uniq_paths)))
+        stop("Something is wrong in the naming of the directories. ",
+             "Some don't end in large or thumbs")
+    ## because dirname() drops trailing /, calling it on the path remove
+    ## the suffix large/thumbs
+    voucher_paths <- unique(dirname(uniq_paths))
+
+    new_names <- lapply(voucher_paths, function(x) {
+        ## extract all images matching the voucher
+        each_voucher_imgs <- grep(paste0("^", x), all_imgs, value = TRUE)
+        ## make sure the thumbs and the large folder have the same file names/number of images
+        if(!sum(duplicated(basename(each_voucher_imgs))) ==
+           length(each_voucher_imgs)/2)
+            stop("problem with ", x)
+        ## create new file name:
+        ##   FLPK-xxxx_YYYY_UUID.JPG
+        ## voucher number, 4 sequential digit, UUID
+        seq_res <- seq_along(unique(basename(each_voucher_imgs)))
+        new_nm <- paste0(basename(x), "_",
+                         sprintf("%04i", seq_res), "_",
+                         replicate(length(seq_res), uuid::UUIDgenerate()),
+                         ".JPG")
+        ## create full complete name by appending path, and letting
+        ## the magical rules of recycling do their job
+        file.path(dirname(each_voucher_imgs), new_nm)
+    })
+
+    renm <- cbind(all_imgs, unlist(new_names))
+    res <- apply(renm, 1, function(x) file.rename(x[1], x[2]))
+    if (all(res)) {
+        message("Renaming successful! ", sum(res), " photos were renamed.")
+    } else {
+        stop("Something went wrong with the renaming...")
+    }
+    invisible(renm)
 }
