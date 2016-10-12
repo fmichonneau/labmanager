3e8cbd22ab289f51f5f5c3235e99550284be00da
Francois Michonneau <francois.michonneau@gmail.com>
2016-10-12 12:28:34 -0400

    use BOLD API to identify the sequences

--- R/sequences.R
+++ R/sequences.R
@@ -4,6 +4,7 @@ read_seq <- function(x, seq_path) {
     if (! file.exists(ff))
         stop(sQuote(ff), " doesn't exist.")
     res <- readLines(file.path(seq_path, x))
+    stopifnot(identical(length(res), 2L))
     data.frame(voucher_number = gsub("^>", "", res[1]),
                sequence = res[2], stringsAsFactors = FALSE)
 }
@@ -32,4 +33,27 @@ get_sequences_by_phylum <- function(phylum, sample_esu = get_lab("sample_esu"),
         cat(res, sep = "\n", file = fasta_file)
     })
     fasta_file
+}
+
+fetch_hook_bold_seq_id <- function(key, namespace) {
+    stopifnot(!missing(namespace))
+    seq_path <- "~/Documents/plankton-larvae-data/seqs/COI/"
+    message("Looking for ",  key, " in the ", namespace, " BOLD database")
+    seq <- read_seq(key, seq_path)$sequence
+    res <- bold::bold_identify(sequence = seq)
+    res[[1]]
+}
+
+store_bold_seq_id <- function(store_path = "~/Documents/plankton-larvae-data/bold_data_store") {
+    storr::storr_external(storr::driver_rds(path = store_path, mangle_key = FALSE),
+                          fetch_hook_bold_seq_id)
+}
+
+update_bold_seq_id_store <- function(seq_path = "~/Documents/plankton-larvae-data/seqs/COI") {
+    all_seqs <- list.files(path = seq_path)
+    out <- lapply(all_seqs, function(x) {
+        store_bold_seq_id()$get(x, namespace = "COX1")
+        store_bold_seq_id()$get(x, namespace = "COX1_SPECIES")
+    })
+    invisible(out)
 }
